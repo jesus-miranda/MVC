de2bfbcee6af1240c4612950e01c1d682b486bee
Jesús Miranda <jmirandamejia@hotmail.com>
2017-06-07 12:18:15 -0500

    Add files via upload

---
+++ Viaticos_CabeceraMetadata.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Inspinia_MVC5.Models
+{
+    public class Viaticos_CabeceraMetadata
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Usuario")]
+        public int IdUsuario { get; set; }
+
+        [Display(Name = "Valor Solicitado")]
+        //[RegularExpression(@"^\$?\d+(\,(\d{2}))?$", ErrorMessage = "El valor para {0} debe tener el siguiente formato 12345,67")]
+        public decimal ValorSolicitado { get; set; }
+
+        [DefaultValue(0)]
+        [Range(0, Double.PositiveInfinity)]
+        [Display(Name = "Valor Aprobado")]
+        //[RegularExpression(@"^\$?\d+(\,(\d{2}))?$", ErrorMessage = "El valor para {0} debe tener el siguiente formato 12345,67")]
+        public decimal ValorAprobado { get; set; }
+
+        [Display(Name = "Número Semana")]
+        [Range(1, 52, ErrorMessage = "El valor para {0} debe estar entre {1} and {2}.")]
+        public int NumeroSemana { get; set; }
+
+        [Display(Name = "Fecha Inicio")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaInicio { get; set; }
+
+        [Display(Name = "Fecha Fin")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime FechaFin { get; set; }
+
+        [Display(Name = "Fecha Registro")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime FechaRegistro { get; set; }
+
+        [StringLength(50, ErrorMessage= "Se aceptan máximo {1} caracteres")]
+        public string DestinoViaje { get; set; }
+
+        [DefaultValue("VIA-")]
+        [StringLength(10)]
+        public string Secuencial { get; set; }
+
+        public int Estado { get; set; }
+    }
+
+    [MetadataType(typeof(Viaticos_CabeceraMetadata))]
+    public partial class Viaticos_Cabecera
+    {
+
+    }
+}
---
+++ Viaticos_DetalleMetadata.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Inspinia_MVC5.Models
+{
+    public class Viaticos_DetalleMetadata
+    {
+        public int Id { get; set; }
+
+        public int IdCabecera { get; set; }
+
+        public int IdConcepto { get; set; }
+
+        [Range(1, 9999, ErrorMessage = "El valor para {0} debe estar entre {1} and {2}.")]
+        public int Cantidad { get; set; }
+
+        [Display(Name = "Precio Unitario")]
+        public decimal PrecioUnitario { get; set; }
+
+        [Display(Name = "Total Gasto")]
+        public decimal TotalGasto { get; set; }
+
+        [Display(Name = "Fecha Ingreso")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy hh:mm:ss}", ApplyFormatInEditMode = true)]
+        public DateTime FechaIngreso { get; set; }
+
+        public int IdUsuarioIngreso { get; set; }
+
+        public int Estado { get; set; }
+    }
+
+    [MetadataType(typeof(Viaticos_DetalleMetadata))]
+    public partial class Viaticos_Detalle
+    {
+
+    }
+}
